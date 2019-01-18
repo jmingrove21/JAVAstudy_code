225265f8b313682a0d7ca92384bd155b2e6527e9
jmingrove21 <min_k21@naver.com>
2019-01-18 15:28:21 +0900

    ch1 code finish

--- JavaStudy_code/src/ch1/Car.java
+++ JavaStudy_code/src/ch1/Car.java
@@ -13,9 +13,29 @@ public class Car {
 	public static void main(String[] args) {
 		Car c=new Car();
 		c.car_info();
+		c.start_car();
 		
+		c.d1.door_info();
+		c.w1.wheel_info();
+		
+		c.d1.color="blue";
+		c.w1.size=20;
+		
+		c.d1.door_info();
+		c.w1.wheel_info();
+		
+		
+		c.stop_car();
 	}
 	public void car_info(){
-		System.out.println("자동차를 만들었습니다!");
+		System.out.println("자동차를 만들었습니다!\n");
 	}
+	
+	public void start_car(){
+		System.out.println("자동차의 시동을 걸었습니다!\n");
+	}
+	public void stop_car(){
+		System.out.println("\n자동차의 시동을 껐습니다!");
+	}
+	
 }
